ff11db832ae4d4cfb9a5c8367248273b604cac9b
MasonN808 <mason.nakamura1@marist.edu>
2021-11-03 22:20:07 -0400

    -Finish Linear Search Implementation      -Start and Finish comparisons of linear search implementation -Start of Binary Search

--- Assignment 3/Linear.java
+++ Assignment 3/Linear.java
@@ -1,5 +1,15 @@
 public class Linear {
-    public String[] linear_search(String[] A){
-
+    int comparisons = 0;
+    public int linear_search(String[] A, String target){
+        int n = A.length;
+        for(int i = 0; i < n; i++){
+            comparisons += 1;
+            if(A[i] == target){
+                // return index of target in array
+                return i;
+            }
+        }
+        // if target is not in array
+        return -1;
     }
 }
--- Assignment 3/Test.java
+++ Assignment 3/Test.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.Arrays;
+import java.util.Random;
 
 public class Test {
 
@@ -24,12 +25,35 @@ public class Test {
             e.printStackTrace();
         }
         String[] copy_words0 = words.clone();
-        //Sort the words
+        //Sort the words using insertion sort
         InserstionSort i_sort = new InserstionSort();
         i_sort.insertionSort(copy_words0);
+        // check if copy_words0 is sorted
 //        for (String i: copy_words0){
 //            System.out.println(i);
 //        }
-
+        // rename copy as sorted
+        String[] sorted_words0 = copy_words0;
+        // initialize random generator to generate random index from length of array
+        Random generator = new Random();
+        String[] random_words = {};
+        for(int i = 0; i < 42; i++){
+            int randomIndex = generator.nextInt(sorted_words0.length);
+            random_words = Arrays.copyOf(random_words, random_words.length + 1); //extends memory
+            random_words[random_words.length-1] = sorted_words0[randomIndex];
+        }
+        // test the 42 random words from sorted_words0
+//        for(String i: random_words){
+//            System.out.println(i);
+//        }
+        // getting comparisons for linear search
+        int total_comparisons = 0;
+        for(String i: random_words){
+            Linear linear = new Linear();
+            linear.linear_search(sorted_words0, i);
+            System.out.println(linear.comparisons);
+            total_comparisons += linear.comparisons;
+        }
+        System.out.println("Average number of Comparisons:" + total_comparisons/42);
     }
 }
