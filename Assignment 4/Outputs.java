8535ba7696ce66e42cf366ac0d0160e11c44ab8a
MasonN808 <mason.nakamura1@marist.edu>
2021-11-17 17:41:21 -0500

    Prints all matrices without bugs.  Working on Adjacency lists.

--- Assignment 4/Outputs.java
+++ Assignment 4/Outputs.java
@@ -3,16 +3,23 @@ public class Outputs {
     /**
      * This method is used to print all adjacency matrices given the lines in the file
      * @param lines an array of every line in the file
-     * @return prints matrices
      */
     public void adjacency_matrix(String[] lines) {
-        int index_start = 0;
+        // use these indices throughout for loops
+        int index_start;
         int index_end = 0;
+        // for printing purposes
+        int graph_id = 0;
+        // for vertex starting index
+        boolean indexIs0 = false;
         while (index_end <= lines.length) {
+            graph_id += 1;
+            index_start = index_end;
             // don't need rows and columns since adjacency matrix is symmetric ==> rows = columns
             int rows_columns = 0;
             // get indices for for loop for edges
-            for (String line : lines) {
+            for (int i = index_start; i < lines.length; i++) {
+                String line = lines[i];
                 index_start += 1;
                 // case for line is blank;
                 if (line.isBlank()) {
@@ -28,12 +35,17 @@ public class Outputs {
                 String[] words = line.split(" ");
                 // case for declaring new graph
                 if (words[0].equals("new")) {
+                    // print graph_id
+                    System.out.println("Graph Number: " + graph_id);
                     //skip the line
                     continue;
                 }
                 // case for adding vertex
                 if (words[0].equals("add") & words[1].equals("vertex")) {
                     rows_columns += 1;
+                    if (words[2].equals("0")){
+                        indexIs0 = true;
+                    }
                     continue;
                 }
                 //check if hit edge case
@@ -43,11 +55,16 @@ public class Outputs {
                 }
 
             }
+
             // create instance of matrix given number of vertices declared
             index_end = index_start + 1;
             int[][] matrix = new int[rows_columns][rows_columns];
             // use index_start to continue in the for loop
             for (int i = index_start; i < index_end; i++) {
+                // case if reached to end of file/(array of lines)
+                if (lines.length <= index_end) {
+                    break;
+                }
                 String line = lines[i];
                 String[] words = line.split(" ");
                 // check if line is blank; if so, move to next graph
@@ -58,12 +75,19 @@ public class Outputs {
                 if (words[0].equals("add") & words[1].equals("edge")) {
                     index_end += 1;
                     // use Integer.parseInt to convert string to int
-                    // subtract 1 to keep indices same
-                    matrix[Integer.parseInt(words[2]) - 1][Integer.parseInt(words[4]) - 1] = 1;
-                    // do it twice for the symmetric matrix with indices flipped
-                    matrix[Integer.parseInt(words[4]) - 1][Integer.parseInt(words[2]) - 1] = 1;
+
+                    if (indexIs0){// if index starts at 0 don't subtract 1
+                        matrix[Integer.parseInt(words[2])][Integer.parseInt(words[4])] = 1;
+                        // do it twice for the symmetric matrix with indices flipped
+                        matrix[Integer.parseInt(words[4])][Integer.parseInt(words[2])] = 1;
+                    }else{// subtract 1 to keep indices same
+                        matrix[Integer.parseInt(words[2]) - 1][Integer.parseInt(words[4]) - 1] = 1;
+                        // do it twice for the symmetric matrix with indices flipped
+                        matrix[Integer.parseInt(words[4]) - 1][Integer.parseInt(words[2]) - 1] = 1;
+                    }
                 }
             }
+
             for (int i = 0; i < matrix.length; i++) {
                 for (int j = 0; j < matrix[i].length; j++) {
                     System.out.print(matrix[i][j] + " ");
