73463a3bbc378f24295ae36728745a5a6ca5c027
MasonN808 <mason.nakamura1@marist.edu>
2021-11-17 15:17:38 -0500

    Configuring method to print ALL matrices --> Many bugs

--- Assignment 4/Outputs.java
+++ Assignment 4/Outputs.java
@@ -5,58 +5,72 @@ public class Outputs {
      * @param lines an array of every line in the file
      * @return prints matrices
      */
-    public int[][] adjacency_matrix(String[] lines){
-        // don't need rows and columns since adjacency matrix is symmetric ==> rows = columns
-        int rows_columns = 0;
-        // get indices for for loop for edges
+    public void adjacency_matrix(String[] lines) {
         int index_start = 0;
-        for (String line : lines) {
-            index_start += 1;
-            // case for line is a comment
-            if (line.charAt(1) == '-' & line.charAt(0) == '-') {
-                // skip the line
-                continue;
-            }
-            // make an array of the words from the line
-            String[] words = line.split(" ");
-            // case for declaring new graph
-            if (words[0].equals("new")) {
-                //skip the line
-                continue;
-            }
-            // case for adding vertex
-            if (words[0].equals("add") & words[1].equals("vertex")) {
-                rows_columns += 1;
-                continue;
-            }
-            //check if hit edge case
-            if (words[0].equals("add") & words[1].equals("edge")) {
-                index_start -=1;
-                break;
-            }
+        int index_end = 0;
+        while (index_end <= lines.length) {
+            // don't need rows and columns since adjacency matrix is symmetric ==> rows = columns
+            int rows_columns = 0;
+            // get indices for for loop for edges
+            for (String line : lines) {
+                index_start += 1;
+                // case for line is blank;
+                if (line.isBlank()) {
+                    // skip the line
+                    continue;
+                }
+                // case for line is a comment
+                if (line.charAt(1) == '-' & line.charAt(0) == '-') {
+                    // skip the line
+                    continue;
+                }
+                // make an array of the words from the line
+                String[] words = line.split(" ");
+                // case for declaring new graph
+                if (words[0].equals("new")) {
+                    //skip the line
+                    continue;
+                }
+                // case for adding vertex
+                if (words[0].equals("add") & words[1].equals("vertex")) {
+                    rows_columns += 1;
+                    continue;
+                }
+                //check if hit edge case
+                if (words[0].equals("add") & words[1].equals("edge")) {
+                    index_start -= 1;
+                    break;
+                }
 
-        }
-        // create instance of matrix given number of vertices declared
-        int index_end = index_start + 1;
-        int[][] matrix = new int[rows_columns][rows_columns];
-        // use index to continue in the for loop
-        for (int i = index_start; i < index_end; i++) {
-            String line = lines[i];
-            String[] words = line.split(" ");
-            // case for adding edge
-            System.out.println(line);
-            if (line.isBlank()){
-                break;
             }
-            if (words[0].equals("add") & words[1].equals("edge")){
-                index_end += 1;
-                // use Integer.parseInt to convert string to int
-                // subtract 1 to keep indices same
-                matrix[Integer.parseInt(words[2])-1][Integer.parseInt(words[4])-1] = 1;
-                // do it twice for the symmetric matrix with indices flipped
-                matrix[Integer.parseInt(words[4])-1][Integer.parseInt(words[2])-1] = 1;
+            // create instance of matrix given number of vertices declared
+            index_end = index_start + 1;
+            int[][] matrix = new int[rows_columns][rows_columns];
+            // use index_start to continue in the for loop
+            for (int i = index_start; i < index_end; i++) {
+                String line = lines[i];
+                String[] words = line.split(" ");
+                // check if line is blank; if so, move to next graph
+                if (line.isBlank()) {
+                    break;
+                }
+                // case for adding edge
+                if (words[0].equals("add") & words[1].equals("edge")) {
+                    index_end += 1;
+                    // use Integer.parseInt to convert string to int
+                    // subtract 1 to keep indices same
+                    matrix[Integer.parseInt(words[2]) - 1][Integer.parseInt(words[4]) - 1] = 1;
+                    // do it twice for the symmetric matrix with indices flipped
+                    matrix[Integer.parseInt(words[4]) - 1][Integer.parseInt(words[2]) - 1] = 1;
+                }
             }
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[i].length; j++) {
+                    System.out.print(matrix[i][j] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
         }
-        return matrix;
     }
 }
--- Assignment 4/Test.java
+++ Assignment 4/Test.java
@@ -28,13 +28,13 @@ public class Test {
         String[] copy_words0 = lines.clone();
 
         Outputs out = new Outputs();
-        int[][] matrix = out.adjacency_matrix(copy_words0);
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
-        }
+        out.adjacency_matrix(copy_words0);
+//        for (int i = 0; i < matrix.length; i++) {
+//            for (int j = 0; j < matrix[i].length; j++) {
+//                System.out.print(matrix[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
 
     }
 }
