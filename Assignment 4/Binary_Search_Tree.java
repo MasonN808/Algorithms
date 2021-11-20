cbed1e7e462c117012d4764a9a535982b8cb3d42
MasonN808 <mason.nakamura1@marist.edu>
2021-11-19 21:39:40 -0500

    Making linked objects work - In progress (attribute processed not working). Binary_Search_Tree -> Finished traversal ---> Finished Working on magicitems-find-in-bst.txt

--- Assignment 4/Binary_Search_Tree.java
+++ Assignment 4/Binary_Search_Tree.java
@@ -2,47 +2,70 @@ public class Binary_Search_Tree {
     int i = 0;
     String path;
     int comparisons;
+//    Node first_root;
     public Node populateBST(Node root, String word){
 
         // if there is no root to begin with or it hits a terminal node
         if (root == null){
-            comparisons += 1;
+//            comparisons += 1;
             Node node = new Node();
             node.name = word;
 //            System.out.println(node.name +": " + path);
-            System.out.println(" " + "Comparisons: " + comparisons);
+//            System.out.println(" " + "Comparisons: " + comparisons);
             return node;
         }
         // go down the left side if word <= root.name
         else if (word.compareTo(root.name) >= 0){
-            comparisons += 1;
+//            comparisons += 1;
             root.left = populateBST(root.left, word);
 //            path += " " + "L";
             System.out.print(" " + "L");
         }
         else{ // go down right side if word > root.name
-            comparisons += 1;
+//            comparisons += 1;
             root.right = populateBST(root.right, word);
 //            path += " " + "R";
             System.out.print(" " + "R");
         }
-        System.out.println();
-        comparisons = 0;
+//        System.out.println();
+//        comparisons = 0;
         return root;
     }
 
     public Node makeBST(String[] words)
     {
         Node root = null;
+        boolean gotroot = false;
         // go through all words and populate BST using method populateBST
         for (String word: words) {
             //reset path
 //            path = "";
+            if (!gotroot){
+                gotroot = true;
+                Node temp_first_root = new Node();
+                temp_first_root.name = word;
+//                first_root = temp_first_root;
+            }
             System.out.println(word + ": ");
             root = populateBST(root, word);
+            System.out.println();
         }
         // if there are no words
         return root;
+    }
+
+    public void inorder(Node root)
+    {
+        if (root == null) {
+            return;
+        }
+
+        // go to the left child
+        inorder(root.left);
+        // print the current root name
+        System.out.println(root.name);
+        // go to the right child
+        inorder(root.right);
     }
 
 }
--- Assignment 4/Test.java
+++ Assignment 4/Test.java
@@ -38,7 +38,7 @@ public class Test {
         // Binary Search Tree population and traversal
         String[] lines1 = {};
         // Read line by line the txt file using File reader
-        String fileName1 = "Assignment 4/magicitems-find-in-bst.txt";  //REMEMBER TO NOT HARDCODE
+        String fileName1 = "Assignment 4/magicitems.txt";  //REMEMBER TO NOT HARDCODE
         File file1 = new File(fileName1);
         try {
             FileReader fr = new FileReader(file1);
@@ -56,12 +56,37 @@ public class Test {
             e.printStackTrace();
         }
 
+        String[] lines2 = {};
+        // Read line by line the txt file using File reader
+        String fileName2 = "Assignment 4/magicitems-find-in-bst.txt";  //REMEMBER TO NOT HARDCODE
+        File file2 = new File(fileName2);
+        try {
+            FileReader fr = new FileReader(file2);
+            BufferedReader br = new BufferedReader(fr);
+            String line;
+            while ((line = br.readLine()) != null) {
+                // add strings from txt file line by line into array words
+                lines2 = Arrays.copyOf(lines2, lines2.length + 1); //extends memory
+                lines2[lines2.length - 1] = line; //adds line to extra memory
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // copy_words2 -> magicitems-find-in-bst.txt
+        // copy_words1 -> magicitems.txt
+        String[] copy_words2 = lines2.clone();
         String[] copy_words1 = lines1.clone();
 
         Binary_Search_Tree bst = new Binary_Search_Tree();
         System.out.println("BINARY SEARCH TREE POPULATING: ");
-        bst.makeBST(copy_words1);
+        Node root = bst.makeBST(copy_words1);
         System.out.println("BINARY SEARCH TREE IN-ORDER TRAVERSAL: ");
+        bst.inorder(root);
+
 
 
     }
