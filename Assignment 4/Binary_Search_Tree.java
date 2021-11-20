e72dbc7ac989f80583f228bde586f83c46e89cd1
MasonN808 <mason.nakamura1@marist.edu>
2021-11-19 19:23:33 -0500

    Making linked objects work - In progress (attribute processed not working). Finished Binary_Search_Tree -> now testing begins.

--- Assignment 4/Binary_Search_Tree.java
+++ Assignment 4/Binary_Search_Tree.java
@@ -1,19 +1,31 @@
 public class Binary_Search_Tree {
-
-    public void BST(String[] words){
-        Node root = new Node();
-        root.name = words[0];
-        int index = 1;
-        while (index < words.length){
-
-            if (words[index].compareTo(root.name) > 0){
-                Node node = new Node();
-                node.name = words[index];
-                root.left = node;
-            }
-            index += 1;
-
-
+    int i = 0;
+    public Node populateBST(Node root, String word){
+        // if there is no root to begin with or it hits a terminal node
+        if (root == null){
+            Node node = new Node();
+            node.name = word;
+            return node;
         }
+        // go down the left side if word <= root.name
+        else if (word.compareTo(root.name) >= 0){
+            root.left = populateBST(root.left, word);
+        }
+        else{ // go down right side if word > root.name
+            root.right = populateBST(root.right, word);
+        }
+        return root;
     }
+
+    public Node makeBST(String[] words)
+    {
+        Node root = null;
+        // go through all words and populate BST using method populateBST
+        for (String word: words) {
+            root = populateBST(root, word);
+        }
+        // if there are no words
+        return root;
+    }
+
 }
