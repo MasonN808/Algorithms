2deb8a17b2bc90e2d6ee729088d597340108e1be
MasonN808 <mason.nakamura1@marist.edu>
2021-11-19 20:55:24 -0500

    Making linked objects work - In progress (attribute processed not working). Finished Binary_Search_Tree -> Works with output.  Now working on traversal.

--- Assignment 4/Binary_Search_Tree.java
+++ Assignment 4/Binary_Search_Tree.java
@@ -1,19 +1,26 @@
 public class Binary_Search_Tree {
     int i = 0;
+    String path;
     public Node populateBST(Node root, String word){
         // if there is no root to begin with or it hits a terminal node
         if (root == null){
             Node node = new Node();
             node.name = word;
+//            System.out.println(node.name +": " + path);
             return node;
         }
         // go down the left side if word <= root.name
         else if (word.compareTo(root.name) >= 0){
             root.left = populateBST(root.left, word);
+//            path += " " + "L";
+            System.out.print(" " + "L");
         }
         else{ // go down right side if word > root.name
             root.right = populateBST(root.right, word);
+//            path += " " + "R";
+            System.out.print(" " + "R");
         }
+        System.out.println();
         return root;
     }
 
@@ -22,6 +29,9 @@ public class Binary_Search_Tree {
         Node root = null;
         // go through all words and populate BST using method populateBST
         for (String word: words) {
+            //reset path
+//            path = "";
+            System.out.println(word + ": ");
             root = populateBST(root, word);
         }
         // if there are no words
--- Assignment 4/Test.java
+++ Assignment 4/Test.java
@@ -25,14 +25,41 @@ public class Test {
             e.printStackTrace();
         }
 
-        String[] copy_words0 = lines.clone();
+        String[] copy_graphs0 = lines.clone();
 
         Outputs out = new Outputs();
         // outputs matrices
-//        out.adjacency_matrix(copy_words0);
+//        out.adjacency_matrix(copy_graphs0);
         // outputs adjacency lists
-//        out.adjacency_list(copy_words0);
-        out.linked_objects(copy_words0);
+//        out.adjacency_list(copy_graphs0);
+//        out.linked_objects(copy_graphs0);
+
+
+        // Binary Search Tree population and traversal
+        String[] lines1 = {};
+        // Read line by line the txt file using File reader
+        String fileName1 = "Assignment 4/magicitems-find-in-bst.txt";  //REMEMBER TO NOT HARDCODE
+        File file1 = new File(fileName1);
+        try {
+            FileReader fr = new FileReader(file1);
+            BufferedReader br = new BufferedReader(fr);
+            String line;
+            while ((line = br.readLine()) != null) {
+                // add strings from txt file line by line into array words
+                lines1 = Arrays.copyOf(lines1, lines1.length + 1); //extends memory
+                lines1[lines1.length - 1] = line; //adds line to extra memory
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String[] copy_words1 = lines1.clone();
+
+        Binary_Search_Tree bst = new Binary_Search_Tree();
+        bst.makeBST(copy_words1);
 
     }
 }
