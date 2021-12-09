854bbeca4e40693da294c3d95d226487546ea090
MasonN808 <mason.nakamura1@marist.edu>
2021-12-09 17:54:01 -0500

    Made an adjaceny matrix generator for the SSSP algo.

--- Assignment 5/LinkedObjects.java
+++ Assignment 5/LinkedObjects.java
@@ -145,4 +145,105 @@ public class LinkedObjects {
 
         }
     }
+
+    public void adjacency_matrix(String[] lines) {
+        // use these indices throughout for loops
+        int index_start;
+        int index_end = 0;
+        // for printing purposes
+        int graph_id = 0;
+        // for vertex starting index
+        boolean indexIs0 = false;
+        while (index_end <= lines.length) {
+            graph_id += 1;
+            index_start = index_end;
+            // don't need rows and columns since adjacency matrix is symmetric ==> rows = columns
+            int rows_columns = 0;
+            // get indices for for loop for edges
+            for (int i = index_start; i < lines.length; i++) {
+                String line = lines[i];
+                index_start += 1;
+                // case for line is blank;
+                if (line.isBlank()) {
+                    // skip the line
+                    continue;
+                }
+                // case for line is a comment
+                if (line.charAt(1) == '-' & line.charAt(0) == '-') {
+                    // skip the line
+                    continue;
+                }
+                // make an array of the words from the line
+                String[] words = line.split(" ");
+                // case for declaring new graph
+                if (words[0].equals("new")) {
+                    // print graph_id
+                    System.out.println("Graph Number: " + graph_id);
+                    //skip the line
+                    continue;
+                }
+                // case for adding vertex
+                if (words[0].equals("add") & words[1].equals("vertex")) {
+                    rows_columns += 1;
+                    if (words[2].equals("0")) {
+                        indexIs0 = true;
+                    }
+                    continue;
+                }
+                //check if hit edge case
+                if (words[0].equals("add") & words[1].equals("edge")) {
+                    index_start -= 1;
+                    break;
+                }
+
+            }
+
+            // create instance of matrix given number of vertices declared
+            index_end = index_start + 1;
+            int[][] matrix = new int[rows_columns][rows_columns];
+            // use index_start to continue in the for loop
+            for (int i = index_start; i < index_end; i++) {
+                // case if reached to end of file/(array of lines)
+                if (lines.length < index_end) {
+                    break;
+                }
+                String line = lines[i];
+                String[] words = line.split(" ");
+                // check if line is blank; if so, move to next graph
+                if (line.isBlank()) {
+                    break;
+                }
+
+                // check if there are two spaces between the weight and the connecting vertex
+                // if so, move up an index
+                if (words[5].equals("")){
+                    words[5] = words[6];
+                }
+
+                // case for adding edge
+                if (words[0].equals("add") & words[1].equals("edge")) {
+                    index_end += 1;
+                    // use Integer.parseInt to convert string to int
+
+                    if (indexIs0) {// if index starts at 0 don't subtract 1
+                        matrix[Integer.parseInt(words[2])][Integer.parseInt(words[4])] = Integer.parseInt(words[5]);
+//                        // do it twice since undirected
+//                        matrix[Integer.parseInt(words[4])][Integer.parseInt(words[2])] = 1;
+                    } else {// subtract 1 to keep indices same
+                        matrix[Integer.parseInt(words[2]) - 1][Integer.parseInt(words[4]) - 1] = Integer.parseInt(words[5]);
+//                        // do it twice since undirected
+//                        matrix[Integer.parseInt(words[4]) - 1][Integer.parseInt(words[2]) - 1] = 1;
+                    }
+                }
+            }
+
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[i].length; j++) {
+                    System.out.print(matrix[i][j] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
 }
