eec0f4e1d16e2d7484fb3c110d47d9d4f5fa6268
MasonN808 <mason.nakamura1@marist.edu>
2021-12-12 22:45:05 -0500

    Took into account cases for the expected values in the pooled testing. Still short on expected tests.

--- Final Project/Testing.java
+++ Final Project/Testing.java
@@ -1,16 +1,22 @@
 public class Testing {
     public static void main(String[] args) {
         Testing_Protocol simulation = new Testing_Protocol();
-        simulation.pooled(1000, 8, .02);
+        simulation.pooled(1000, 8, .02, true);
+
+        // DEBUGGING
+        // TODO: Find the bug that isn't making the simulated tests != expected tests
         System.out.println(simulation.total_tests);
-        System.out.println(simulation.case_tests);
+        System.out.println(simulation.case1_tests);
+        System.out.println(simulation.case2_tests);
+        System.out.println(simulation.case3_tests);
+
 
         // average the number of tests for n number of simulations/iterations
         int total_total_tests = 0;
         final int ITERATIONS = 1000;
         for (int i = 0; i < ITERATIONS; i++){
             Testing_Protocol simulation1 = new Testing_Protocol();
-            simulation1.pooled(1000, 8, .02);
+            simulation1.pooled(1000, 8, .02, false);
             total_total_tests += simulation1.total_tests;
         }
         System.out.println("Averaged Tests over " + ITERATIONS + " iterations: " + total_total_tests/ITERATIONS);
--- Final Project/Testing_Protocol.java
+++ Final Project/Testing_Protocol.java
@@ -4,17 +4,25 @@ public class Testing_Protocol {
 
     // number of tests needed
     int total_tests = 0;
-    int case_tests = 0;
+    int case1_tests = 0;
+    int case2_tests = 0;
+    int case3_tests = 0;
+    int case1 = 0;
+    int case2 = 0;
+    int case3 = 0;
+
+
 
     /**
      * Gets the first and last name of this Student.
      * @param people The number of people to be tested
      * @param group_size The group size for initial pooled test
      * @param infection_rate The rate at which the population is being infected
+     * @param show_expected Prints the expected number of occurrences of each case and number of tests
      */
-    public void pooled(int people, int group_size, double infection_rate){
+    public void pooled(int people, int group_size, double infection_rate, boolean show_expected){
         // Define number of groups
-        double num_groups = people/group_size;
+        double num_groups = (double)people/group_size;
         // Initialize an array of length "people"
         // All values in "infected" are set to false by default
         boolean[] infected = new boolean[people];
@@ -49,6 +57,7 @@ public class Testing_Protocol {
             loops += 1;
             // test the whole pool
             total_tests += 1;
+            case1_tests += 1;
             // linear search through pool
             for(int i = start_index; i < end_index; i++){
                 // if someone is infected, then split pool in two
@@ -59,29 +68,33 @@ public class Testing_Protocol {
 //                    System.out.println(start_index + ":" + half_end_index + ":" + end_index);
                     // test first half of test pool
                     total_tests += 1;
+                    case2_tests += 1;
                     for(int j = start_index; j < half_end_index; j++){
-                        // if someone is infected, then test everyone in the half pool size
+                        // test if someone is infected in half pool size
                         if(infected[j]){
                             // test everyone in the half pool size
                             for(int k = start_index; k < half_end_index; k++){
                                 total_tests += 1;
-                                case_tests += 1;
+                                case3_tests += 1;
                             }
                         }
                     }
                     // test second half of test pool
                     total_tests += 1;
+                    case2_tests += 1;
+                    // TODO HERE
+//                    System.out.println(start_index + ":" + half_end_index + ":" + end_index);
                     for(int j = half_end_index + 1; j < end_index; j++){
-                        // if someone is infected, then test everyone in the half pool size
+                        // test if someone is infected in half pool size
                         if(infected[j]){
                             // test everyone in the half pool size
-                            for(int k = half_end_index + 1; k < half_end_index; k++){
+                            for(int k = half_end_index + 1; k < end_index; k++){
                                 total_tests += 1;
-                                case_tests += 1;
+                                case3_tests += 1;
                             }
                         }
                     }
-                    // break out of outer for loop to stop linear search, since the pool has already been tested on
+                    // break out of outer for loop to stop linear search, since the pool of size group_size has already been tested on
                     break;
                 }
 
@@ -90,43 +103,53 @@ public class Testing_Protocol {
             start_index = end_index;
             end_index = group_size*loops;
         }
-        // Find the expected percentage of occurrences
-        double case1 = Math.pow(1-infection_rate,group_size);
-        double case3 = Math.pow(infection_rate,2);
-        double case2 = 1-case1-case3;
 
-        // Find the expected number of tests needed in pooled testing
-        int test1 = (int)Math.ceil(num_groups * case1);
-        int test2 = (int)Math.ceil(num_groups * case2) * 7;
-        int test3 = (int)Math.ceil(num_groups * case3) * 11;
+        if (show_expected) {
+            // Find the expected percentage of occurrences
+            double case1 = Math.pow(1 - infection_rate, group_size);
+            double case3 = Math.pow(infection_rate, 2);
+            double case2 = 1 - case1 - case3;
 
-        // For printing a certain number of decimals
-        DecimalFormat numberFormat = new DecimalFormat("#.00000");
-        System.out.println("Case 1");
-        System.out.println("Expected Percentage of Total Occurrences: " + numberFormat.format(case1));
-        // take ceiling to err on the side of caution and convert from double to integer
-        System.out.println("Expected Number of Tests: " + test1);
+            // Find the expected number of tests needed in pooled testing
+            int test1 = (int) Math.ceil(num_groups * case1);
+            // Initialize test2 and test3
+            // Since taking the ceiling of a number less than one will be one
+            int test2, test3;
+            if (num_groups * case2 < 1){
+                test2 = (int) Math.ceil(num_groups * case2) * 7;
+            }
+            else{
+                test2 = (int) Math.ceil(num_groups * case2 * 7);
+            }
+            // since taking the ceiling of a number less than one will be one
+            if (num_groups * case3 < 1){
+                test3 = (int) Math.ceil(num_groups * case3) * 11;
+            }
+            else{
+                test3 = (int) Math.ceil(num_groups * case3 * 11);
+            }
 
-        System.out.println("------------");
-        System.out.println("Case 2");
-        System.out.println("Expected Number of Occurrences: " + numberFormat.format(case2));
-        System.out.println("Expected Number of Tests: " + test2);
+            // For printing a certain number of decimals
+            DecimalFormat numberFormat = new DecimalFormat("#.00000");
+            System.out.println("Case 1");
+            System.out.println("Expected Percentage of Total Occurrences: " + numberFormat.format(case1));
+            // take ceiling to err on the side of caution and convert from double to integer
+            System.out.println("Expected Number of Tests: " + test1);
 
-        System.out.println("------------");
-        System.out.println("Case 3");
-        System.out.println("Expected Number of Occurrences: " + numberFormat.format(case3));
-        System.out.println("Expected Number of Tests: " + test3);
+            System.out.println("------------");
+            System.out.println("Case 2");
+            System.out.println("Expected Number of Occurrences: " + numberFormat.format(case2));
+            System.out.println("Expected Number of Tests: " + test2);
 
-        System.out.println("------------");
-        System.out.println("Total Expected Number of Tests: " + (test1 + test2 + test3));
+            System.out.println("------------");
+            System.out.println("Case 3");
+            System.out.println("Expected Number of Occurrences: " + numberFormat.format(case3));
+            System.out.println("Expected Number of Tests: " + test3);
 
+            System.out.println("------------");
+            System.out.println("Total Expected Number of Tests: " + (test1 + test2 + test3));
+            System.out.println("------------");
 
-
-
-
-
-//        for (boolean j: infected){
-//            System.out.println(j);
-//        }
+        }
     }
 }
