5b496cd451b33d929eeb726d669efe0ab55742ec
MasonN808 <mason.nakamura1@marist.edu>
2021-12-12 23:54:03 -0500

    Solved case when people % group_size != 0

--- Final Project/Testing.java
+++ Final Project/Testing.java
@@ -5,11 +5,7 @@ public class Testing {
 
         // DEBUGGING
         // TODO: Find the bug that isn't making the simulated tests != expected tests
-        System.out.println(simulation.total_tests);
-        System.out.println(simulation.case1_tests);
-        System.out.println(simulation.case2_tests);
-        System.out.println(simulation.case3_tests);
-
+        System.out.println("Simulated total Tests: " + simulation.total_tests);
 
         // average the number of tests for n number of simulations/iterations
         int total_total_tests = 0;
@@ -18,6 +14,7 @@ public class Testing {
             Testing_Protocol simulation1 = new Testing_Protocol();
             simulation1.pooled(1000, 8, .02, false);
             total_total_tests += simulation1.total_tests;
+//            System.out.println(simulation1.total_tests);
         }
         System.out.println("Averaged Tests over " + ITERATIONS + " iterations: " + total_total_tests/ITERATIONS);
 
--- Final Project/Testing_Protocol.java
+++ Final Project/Testing_Protocol.java
@@ -4,12 +4,12 @@ public class Testing_Protocol {
 
     // number of tests needed
     int total_tests = 0;
-    int case1_tests = 0;
-    int case2_tests = 0;
-    int case3_tests = 0;
-    int case1 = 0;
-    int case2 = 0;
-    int case3 = 0;
+//    int case1_tests = 0;
+//    int case2_tests = 0;
+//    int case3_tests = 0;
+//    int case1 = 0;
+//    int case2 = 0;
+//    int case3 = 0;
 
 
 
@@ -23,6 +23,7 @@ public class Testing_Protocol {
     public void pooled(int people, int group_size, double infection_rate, boolean show_expected){
         // Define number of groups
         double num_groups = (double)people/group_size;
+        boolean isdivisible = people % group_size == 0;
         // Initialize an array of length "people"
         // All values in "infected" are set to false by default
         boolean[] infected = new boolean[people];
@@ -33,14 +34,11 @@ public class Testing_Protocol {
         for(int i = 0; i < infected_end; i++){
             infected[i] = true;
         }
+
         // Knuth shuffle array, infected
         Shuffle shuffle = new Shuffle();
         shuffle.shuffle(infected);
-//        for (boolean i: infected){
-//            if(i){
-//                System.out.print(1);
-//            }else System.out.print(0);
-//        }
+
         // Check the pools for infected of size "group_size"
         // if pool has infected, split pool in two and test if each pool has infected
         // then do individual test for each two pools that has infected
@@ -48,60 +46,86 @@ public class Testing_Protocol {
         int start_index = 0;
         // end index for pooled group
         // delete 1 to group_size to convert from type size to type index
-        int end_index = group_size-1;
+        int end_index = group_size;
         // number of loops of pooled testing in the population of people
         int loops = 1;
-        //TODO implement recursion
+
+        //TESTING
+//        infected[0] = true;
+//        infected[5] = true;
+//        infected[6] = true;
+//        infected[7] = true;
+//        infected[8] = true;
+//        infected[12] = true;
+
         //TODO take into account that the last pooled group is not of length group_size
         while(start_index < people){
             loops += 1;
             // test the whole pool
             total_tests += 1;
-            case1_tests += 1;
             // linear search through pool
-            for(int i = start_index; i < end_index; i++){
+            for (int i = start_index; i < end_index; i++) {
                 // if someone is infected, then split pool in two
-                if(infected[i]){
+                if (infected[i]) {
                     //TODO make sure half_end_index takes into account odd test size (not needed for this project)
                     //add 1 to end_index to convert from type index to type size, then delete 1 to convert to type index
-                    int half_end_index = end_index-group_size/2;
-//                    System.out.println(start_index + ":" + half_end_index + ":" + end_index);
+                    int half_end_index = end_index - group_size / 2;
                     // test first half of test pool
                     total_tests += 1;
-                    case2_tests += 1;
-                    for(int j = start_index; j < half_end_index; j++){
+                    for (int j = start_index; j < half_end_index; j++) {
                         // test if someone is infected in half pool size
-                        if(infected[j]){
+                        if (infected[j]) {
+//                            //TESTING
+//                            System.out.print("["+ start_index + "," + half_end_index+ "] " );
+//                            //TESTING
+//                            System.out.print("FIRST HALF: ");
                             // test everyone in the half pool size
-                            for(int k = start_index; k < half_end_index; k++){
+                            for (int k = start_index; k < half_end_index; k++) {
                                 total_tests += 1;
-                                case3_tests += 1;
+//                                //TESTING
+//                                System.out.print(k + " ");
                             }
+//                            //TESTING
+//                            System.out.println();
+                            // break out of outer for loop to stop linear search, since the pool of size group_size/2 has already been tested on
+                            break;
                         }
                     }
                     // test second half of test pool
                     total_tests += 1;
-                    case2_tests += 1;
-                    // TODO HERE
-//                    System.out.println(start_index + ":" + half_end_index + ":" + end_index);
-                    for(int j = half_end_index + 1; j < end_index; j++){
+                    for (int j = half_end_index; j < end_index; j++) {
                         // test if someone is infected in half pool size
-                        if(infected[j]){
+                        if (infected[j]) {
+                            // TODO this for-loop only loops three times not four
+//                            //TESTING
+//                            System.out.print("["+ half_end_index + "," + end_index + "] " );
+//                            //TESTING
+//                            System.out.print("SECOND HALF: ");
                             // test everyone in the half pool size
-                            for(int k = half_end_index + 1; k < end_index; k++){
+                            for (int k = half_end_index; k < end_index; k++) {
                                 total_tests += 1;
-                                case3_tests += 1;
+//                                //TESTING
+//                                System.out.print(k + " ");
                             }
+//                            //TESTING
+//                            System.out.println();
+                            // break out of outer for loop to stop linear search, since the pool of size group_size/2 has already been tested on
+                            break;
                         }
                     }
                     // break out of outer for loop to stop linear search, since the pool of size group_size has already been tested on
                     break;
                 }
-
             }
-            // move to next pool
+            // move to next testing pool
             start_index = end_index;
-            end_index = group_size*loops;
+            // Case when people % group_size != 0
+            if (group_size * loops > people){
+                end_index = people;
+            }
+            else{
+                end_index = group_size*loops;
+            }
         }
 
         if (show_expected) {
