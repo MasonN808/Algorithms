69002d8b76b7961fb6f981b3206534d36b31c81f
MasonN808 <mason.nakamura1@marist.edu>
2021-12-14 20:12:16 -0500

    Added more printing statements for differing number of people getting tested

--- Final Project/Testing.java
+++ Final Project/Testing.java
@@ -1,15 +1,77 @@
 public class Testing {
     public static void main(String[] args) {
+        final int ITERATIONS = 100;
+
+        System.out.println();
+        System.out.println();
+        System.out.println("1,000 PEOPLE");
+        System.out.println("------------");
+
         Testing_Protocol simulation = new Testing_Protocol();
         simulation.pooled(1000, 8, .02, true);
         System.out.println("Simulated total Tests: " + simulation.total_tests);
 
         // average the number of tests for n number of simulations/iterations
         int total_total_tests = 0;
-        final int ITERATIONS = 1000;
         for (int i = 0; i < ITERATIONS; i++){
             Testing_Protocol simulation1 = new Testing_Protocol();
             simulation1.pooled(1000, 8, .02, false);
+            total_total_tests += simulation1.total_tests;
+//            System.out.println(simulation1.total_tests);
+        }
+        System.out.println("Averaged Tests over " + ITERATIONS + " iterations: " + total_total_tests/ITERATIONS);
+
+        System.out.println();
+        System.out.println();
+        System.out.println("10,00 PEOPLE");
+        System.out.println("------------");
+
+        simulation.pooled(10000, 8, .02, true);
+        System.out.println("Simulated total Tests: " + simulation.total_tests);
+
+        // average the number of tests for n number of simulations/iterations
+        total_total_tests = 0;
+        for (int i = 0; i < ITERATIONS; i++){
+            Testing_Protocol simulation1 = new Testing_Protocol();
+            simulation1.pooled(10000, 8, .02, false);
+            total_total_tests += simulation1.total_tests;
+//            System.out.println(simulation1.total_tests);
+        }
+        System.out.println("Averaged Tests over " + ITERATIONS + " iterations: " + total_total_tests/ITERATIONS);
+
+        System.out.println();
+        System.out.println();
+        System.out.println("100,000 PEOPLE");
+        System.out.println("------------");
+
+        simulation.pooled(100000, 8, .02, true);
+        System.out.println("Simulated total Tests: " + simulation.total_tests);
+
+        // average the number of tests for n number of simulations/iterations
+        total_total_tests = 0;
+        for (int i = 0; i < ITERATIONS; i++){
+            Testing_Protocol simulation1 = new Testing_Protocol();
+            simulation1.pooled(100000, 8, .02, false);
+            total_total_tests += simulation1.total_tests;
+//            System.out.println(simulation1.total_tests);
+        }
+        System.out.println("Averaged Tests over " + ITERATIONS + " iterations: " + total_total_tests/ITERATIONS);
+
+        System.out.println();
+        System.out.println();
+        System.out.println("1M PEOPLE");
+        System.out.println("------------");
+
+
+
+        simulation.pooled(1000000, 8, .02, true);
+        System.out.println("Simulated total Tests: " + simulation.total_tests);
+
+        // average the number of tests for n number of simulations/iterations
+        total_total_tests = 0;
+        for (int i = 0; i < ITERATIONS; i++){
+            Testing_Protocol simulation1 = new Testing_Protocol();
+            simulation1.pooled(1000000, 8, .02, false);
             total_total_tests += simulation1.total_tests;
 //            System.out.println(simulation1.total_tests);
         }
